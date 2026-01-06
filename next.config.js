c83a052d769edb522c62a05349b6f019bfeaab1f
ididr47-create <ididr47@gmail.com>
2026-01-06 10:44:22 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
